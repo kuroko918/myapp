b6fe999d62700126db71f4e2babb4a309c079c72
kazuki_iwamoto <chocoberry.96@gmail.com>
2020-11-15 16:27:02 +0900

    Update index and  Create login page

---
+++ web/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <button class="button is-primary is-rounded" @click="login">
+      GitHubでログイン
+    </button>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  methods: {
+    async login (): Promise<void> {
+      await this.$store.dispatch('auth/login')
+      this.$router.push({ name: 'index' })
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
