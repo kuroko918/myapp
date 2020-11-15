08f8c93a836f8804cee581d20048eded4eb30dbe
kazuki_iwamoto <chocoberry.96@gmail.com>
2020-11-15 16:25:39 +0900

    Introduce firebase authorization

---
+++ web/store/auth.ts
@@ -0,0 +1,68 @@
+import { Getters, Actions, Mutations, Module } from 'vuex-smart-module'
+import firebase from '~/plugins/firebase'
+
+class AuthState {
+  currentUser: {
+    authToken: string;
+    id: string;
+    name: string;
+    email: string;
+    avater: string;
+  } | null = null;
+}
+
+class AuthMutations extends Mutations<AuthState> {
+  setCurrentUser (user: any): void {
+    if (!user) {
+      this.state.currentUser = null
+      return
+    }
+
+    if (process.server) {
+      this.state.currentUser = user
+      return
+    }
+
+    this.state.currentUser = {
+      authToken: user.ya,
+      id: user.uid,
+      name: user.displayName,
+      email: user.email,
+      avater: user.photoURL
+    }
+  }
+}
+
+class AuthActions extends Actions<AuthState, AuthGetters, AuthMutations> {
+  async login (): Promise<void> {
+    const provider = new firebase.auth.GithubAuthProvider()
+    try {
+      const result = await firebase.auth().signInWithPopup(provider)
+      this.commit('setCurrentUser', result.user)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  async logout (): Promise<void> {
+    try {
+      await firebase.auth().signOut()
+      this.commit('setCurrentUser', null)
+    } catch (error) {
+      alert(error)
+    }
+  }
+}
+
+class AuthGetters extends Getters<AuthState> {
+  isAuthenticated (): boolean {
+    return !!this.state.currentUser
+  }
+}
+
+export default new Module({
+  state: AuthState,
+  getters: AuthGetters,
+  mutations: AuthMutations,
+  actions: AuthActions
+})
