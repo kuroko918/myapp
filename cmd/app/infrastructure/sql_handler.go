588290ab4a485b96f3f2966b97cdb1b7b3fdd68f
kazuki_iwamoto <chocoberry.96@gmail.com>
2020-12-11 21:42:13 +0900

    Update unused func definition

--- cmd/app/infrastructure/sql_handler.go
+++ cmd/app/infrastructure/sql_handler.go
@@ -29,26 +29,10 @@ func (handler *SqlHandler) Delete(value interface{}) *gorm.DB {
 	return handler.Conn.Delete(value)
 }
 
-func (handler *SqlHandler) Exec(sql string, values ...interface{}) *gorm.DB {
-	return handler.Conn.Exec(sql, values...)
-}
-
 func (handler *SqlHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
 	return handler.Conn.Find(out, where...)
 }
 
-func (handler *SqlHandler) First(out interface{}, where ...interface{}) *gorm.DB {
-	return handler.Conn.First(out, where...)
-}
-
-func (handler *SqlHandler) Raw(sql string, values ...interface{}) *gorm.DB {
-	return handler.Conn.Raw(sql, values...)
-}
-
 func (handler *SqlHandler) Save(value interface{}) *gorm.DB {
 	return handler.Conn.Save(value)
-}
-
-func (handler *SqlHandler) Where(query interface{}, args ...interface{}) *gorm.DB {
-	return handler.Conn.Where(query, args...)
 }
