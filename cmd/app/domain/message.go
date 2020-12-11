af5470a26a6603009da5ce1c6ecbfc0d93802dd9
kazuki_iwamoto <chocoberry.96@gmail.com>
2020-12-11 21:41:24 +0900

    Update domain/message

--- cmd/app/domain/message.go
+++ cmd/app/domain/message.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type Message struct {
-	ID        int
-	Content   string
-	UserId    string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `json:"id"`
+	Content   string    `json:"content"`
+	UserId    string    `json:"userId"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type Messages []Message
