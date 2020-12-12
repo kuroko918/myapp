5bab79c84c4266cc713d0b1eda4b9aebacef24ed
kazuki_iwamoto <chocoberry.96@gmail.com>
2020-12-12 16:43:36 +0900

    Fix domain/Message DeletedAt null true

--- cmd/app/domain/message.go
+++ cmd/app/domain/message.go
@@ -8,7 +8,7 @@ type Message struct {
 	UserId    string    `json:"userId"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	DeletedAt time.Time `json:"deletedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
 }
 
 type Messages []Message
