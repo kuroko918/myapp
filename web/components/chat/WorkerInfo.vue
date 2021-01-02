a1c8e8a6d8b5307ce7d5dd3052553e431b31a0a8
kazuki_iwamoto <chocoberry.96@gmail.com>
2021-01-02 22:19:18 +0900

    Create WorkerInfo component

---
+++ web/components/chat/WorkerInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img class="workerIcon" :src="userAvatar">
+    <div>{{ user.name }}</div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue, { PropType } from 'vue'
+import { IUser } from '../../types/models/user'
+
+export default Vue.extend({
+  props: {
+    user: {
+      type: Object as PropType<IUser>,
+      default: () => {}
+    }
+  },
+  computed: {
+    userAvatar (): string {
+      if (!this.user.avatar) { return '/images/flutter.png' }
+
+      return this.user.avatar
+    }
+  }
+})
+</script>
+
+<style>
+.workerIcon {
+  width: 25px;
+  height: 25px;
+}
+</style>
