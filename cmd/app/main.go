39e09f2c0f5f3238a3f4f8340a0a31bed76df166
kazuki_iwamoto <chocoberry.96@gmail.com>
2021-04-01 00:25:19 +0900

    Remove cmd/app directory

--- cmd/app/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/kuroko918/myapp/cmd/app/infrastructure"
-)
-
-func main() {
-	infrastructure.Router.Run(":8080")
-}
--- cmd/app/usecase/user_repository.go
+++
@@ -1,7 +0,0 @@
-package usecase
-
-import "github.com/kuroko918/myapp/cmd/app/domain"
-
-type UserRepository interface {
-	GetOrStore(domain.User) (domain.User, error)
-}
